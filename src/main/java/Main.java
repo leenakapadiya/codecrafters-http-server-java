53b19b3dcdfbf7cf2a7855e0d94c584edcca1e39
Leena <leenapagdar95@gmail.com>
2024-09-22 15:50:20 -0700

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,6 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -15,9 +17,23 @@ public class Main {
        // ensures that we don't run into 'Address already in use' errors
        serverSocket.setReuseAddress(true);
 
-       clientSocket = serverSocket.accept(); // Wait for connection from client.
-       System.out.println("accepted new connection");
-       clientSocket.getOutputStream().write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+       clientSocket = serverSocket.accept(); // Wait for connection from client
+       BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+       String line = bufferedReader.readLine();
+       while(line != null && !line.isEmpty()) {
+           if(line.startsWith("GET")) {
+               String path = line.split(" ")[1];
+               if(path.equals(("/"))) {
+                   clientSocket.getOutputStream().write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+               } else {
+                     clientSocket.getOutputStream().write("HTTP/1.1 404 Not Found\r\n\r\n".getBytes());
+               }
+               break;
+           }
+
+       }
+//       System.out.println("accepted new connection");
+//       clientSocket.getOutputStream().write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
      } catch (IOException e) {
        System.out.println("IOException: " + e.getMessage());
      }
